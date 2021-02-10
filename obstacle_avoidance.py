1fe0d99414038c11311d8746dd94665bf050134d
voidTM <samsong974@gmail.com>
2021-02-09 22:28:52 -0800

    scan drive loop

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -35,11 +35,70 @@ def turn(speed):
         
         fc.turn_right(speed)
 
+
+#scanning
+ANGLE_RANGE = 180
+STEP = 20
+angle_distance = [0,0]
+current_angle = 0
+max_angle = ANGLE_RANGE/2
+min_angle = -ANGLE_RANGE/2
+step_count = ANGLE_RANGE/STEP
+
+# degrees are from max -> min
+scan_list = [0] * 9
+
+
+# does a complete 180 scan:
+
+def full_scan():
+    global scan_list, STEP, step_count
+
+    current_angle = max_angle
+    for i in range(0, step_count, STEP):        
+        scan_list[i] = fc.get_distance_at(current_angle)
+        current_angle -= STEP
+
+
+def drive():
+    
+    go = True
+    current_angle = 0
+    
+    while go:
+        #scan
+        fc.forward(speed)
+
+
+
+
+
+def drive2():
+    speed = 30
+    scan_dist = 20
+    while True:
+        scan_list = fc.scan_step(scan_dist)
+        if not scan_list:
+            continue
+
+        tmp = scan_list[3:7]
+        print(tmp)
+        if tmp != [2,2,2,2]:
+            scan_dist = 50
+            speed = 10
+            
+            
+            fc.turn_right(speed)
+        else:
+            scan_dist = 20
+            speed = 30
+            fc.forward(speed)
+
+
 if __name__ == "__main__":
     try: 
-        forward(10)
-        backup(10)
-        turn(10)
+        full_scan()
+        drive2()
     finally: 
         fc.get_distance_at(0)
         fc.stop()
---
+++ scan.py
@@ -0,0 +1,45 @@
+
+from picar_4wd.speed import Speed
+import RPi.GPIO as GPIO
+import time, math
+import threading
+import picar_4wd as fc
+
+import sys
+import tty
+import termios
+import asyncio
+import time
+
+
+ANGLE_RANGE = 180
+STEP = 20
+us_step = STEP
+angle_distance = [0,0]
+current_angle = 0
+max_angle = ANGLE_RANGE/2
+min_angle = -ANGLE_RANGE/2
+scan_list = [0] * 9
+
+def scan_step(ref):
+    global scan_list, current_angle, us_step
+    current_angle += us_step
+    if current_angle >= max_angle:
+        current_angle = max_angle
+        us_step = -STEP
+    elif current_angle <= min_angle:
+        current_angle = min_angle
+        us_step = STEP
+    status = get_status_at(current_angle, ref1=ref)#ref1
+
+    scan_list.append(status)
+    if current_angle == min_angle or current_angle == max_angle:
+        if us_step < 0:
+            # print("reverse")
+            scan_list.reverse()
+        # print(scan_list)
+        tmp = scan_list.copy()
+        scan_list = []
+        return tmp
+    else:
+        return False
