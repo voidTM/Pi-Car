85d1b1caa747337a93cf7a36a16409996e4be6cc
voidTM <samsong974@gmail.com>
2021-02-17 20:43:52 -0800

    watch out for nearsited

--- roomba.py
+++ roomba.py
@@ -1,98 +1,31 @@
 from picar_4wd.speed import Speed
 import RPi.GPIO as GPIO
 import time, math
-import threading
 import picar_4wd as fc
 
 import sys
-import tty
-import termios
-import asyncio
 import time
 
+import scan as sc
+from odometer import Duodometer
 
+"""
+    The basic driving and avoiding obstacles should be good for steps 1-3
+"""
 # 60 degree is about the closest servos
 #scanning
-ANGLE_RANGE = 180
-STEP = 18
-angle_distance = [0,0]
-current_angle = 0
-max_angle = ANGLE_RANGE/2
-min_angle = -ANGLE_RANGE/2
-step_count = ANGLE_RANGE//STEP
-
-# degrees are from max -> min
-scan_list = [0] * 10
-        
-
-# scans the next angle
-def my_step_scan(ref1, ref2):
-    global scan_list, current_angle, us_step
-    current_angle += us_step
-    # switches directions for angle
-    if current_angle >= max_angle:
-        current_angle = max_angle
-        us_step = -STEP
-    elif current_angle <= min_angle:
-        current_angle = min_angle
-        us_step = STEP
-    
-    dist = get_distance_at(current_angle)
-    status = get_status_at(current_angle, ref1=ref1, ref2= ref2)#ref1
-
-    #scan_list[i] = status
-    return (current_angle, status)
-
-
-
-# 20 should be the minimum distance
 
 def drive():
     speed = 30
-    scan_dist = 20
+    blocked = True
+
+    tracker = Duodometer(4,24)
+    tracker.start()
+    
+    
     
     while True:
-        scan_list = fc.scan_step(scan_dist)
-        if not scan_list:
-            continue
-
-        tmp = scan_list[3:7]
-        print(tmp)
-        if tmp != [2,2,2,2]:
-            scan_dist = 50
-            speed = 10
-            
-
-            fc.turn_right(speed)
-        else:
-            time.sleep(10)
-            scan_dist = 20
-            speed = 30
-            fc.forward(speed)
-
-
-
-# drive and stop
-
-def drive2():
-    speed = 30
-    scan_dist = 35
-    
-    while True:
-        scanned = fc.scan_step(scan_dist)
-        if not scanned:
-            continue
-        print(scanned)
-        if(min(scanned) < 1):
-            fc.stop()
-        
-def drive3():
-    speed = 30
-
-    fc.forward(speed) 
-
-    while True:
-        scan_list = fc.scan_step(35)
+        scan_list = sc.scan_step_dist()
 
 
         if not scan_list:
@@ -100,9 +33,60 @@ def drive3():
         print(scan_list)
         ahead = scan_list[2:8]
         # coast clear full speed ahead        
-        if min(ahead) > 1:
+        if min(ahead) > 20:
             print("Coast Clear")
+            blocked = False
             fc.forward(speed)
+            continue
+
+        print("need to stop")
+
+        fc.stop()
+
+        left = scan_list[:5]
+        right = scan_list[5:]
+
+        # -1 = turn left, 0 forward, 1 turn right
+        direction = 0
+        print(left, right)
+        if(sum(left) > sum(right)):
+            direction = -1
+        elif(sum(left) < sum(right)):
+            direction = 1
+        else:
+            direction = 0
+
+
+        if direction  == -1:
+            print("turning left")
+            fc.turn_left(10)
+        elif direction  == 1:
+            print("turning right")
+            fc.turn_right(10)
+        else:
+            while(fc.get_distance_at(0) < 30):
+                fc.backward(2)
+
+        
+# same as drive 1 but aims to use distance rather than status
+def drive2():
+    speed = 30
+
+    while True:
+        scan_list = sc.scan_step_dist()
+
+        if not scan_list:
+            continue
+        print(scan_list)
+        ahead = scan_list[2:8]
+        # coast clear full speed ahead        
+        if min(ahead) > 35:
+            print("Coast Clear")
+            blocked = False
+            fc.forward(speed)
+            continue
+        elif min(scan_list) > 20:
+            fc.forward(5)
             continue
 
         print("need to stop")
@@ -133,12 +117,12 @@ def drive3():
             while(fc.get_distance_at(0) < 50):
                 fc.backward(2)
 
-        
+    
 
 
 if __name__ == "__main__":
     try: 
-        drive5()
+        drive2()
     finally: 
         fc.get_distance_at(0)
         fc.stop()
--- scan.py
+++ scan.py
@@ -6,15 +6,15 @@ import picar_4wd as fc
 
 
 ANGLE_RANGE = 180
-STEP = 20
+STEP = 18
 us_step = STEP
 angle_distance = [0,0]
 current_angle = 0
 max_angle = ANGLE_RANGE/2
 min_angle = -ANGLE_RANGE/2
 
-scan_list_status = []
-scan_list_dist = []
+scan_status = []
+scan_dist = []
 
 
 def next_step():
@@ -29,7 +29,7 @@ def next_step():
     return current_angle
 
 def scan_step_status(ref1 , ref2):
-    global scan_list_status, current_angle, us_step
+    global scan_status, current_angle, us_step
 
     current_angle += us_step
     if current_angle >= max_angle:
@@ -41,7 +41,7 @@ def scan_step_status(ref1 , ref2):
 
     status = get_status_at(current_angle, ref1=ref1, ref2=ref2)#ref1
 
-    scan_list_status.append(status)
+    scan_status.append(status)
     if current_angle == min_angle or current_angle == max_angle:
         if us_step < 0:
             # print("reverse")
@@ -53,7 +53,7 @@ def scan_step_status(ref1 , ref2):
     else:
         return False
 
-def step_scan_dist():
+def scan_step_dist():
     global scan_dist, current_angle, us_step
     current_angle += us_step
     if current_angle >= max_angle:
@@ -65,7 +65,7 @@ def step_scan_dist():
 
     dist = fc.get_distance_at(current_angle)
 
-    scan_dist.append(status)
+    scan_dist.append(dist)
     if current_angle == min_angle or current_angle == max_angle:
         if us_step < 0:
             # print("reverse")
